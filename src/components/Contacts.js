23d3572a43bc8aaa9babb60e409da7a3af676781
dasharath-491 <khichadedasharath@gmail.com>
2024-01-10 20:41:45 +0530

    mobile css

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -24,7 +24,7 @@ const Contacts = ({ contacts, changeChat }) => {
       {currentUserImage && currentUsername && (
         <Container>
           <div className="brand">
-            <img src={Logo} alt="logo" />
+            <img src={Logo} alt="logo" className="main_logo" />
             <h3>chat-app</h3>
           </div>
           <div className="contacts">
@@ -55,6 +55,7 @@ const Contacts = ({ contacts, changeChat }) => {
               <img
                 src={`data:image/svg+xml;base64,${currentUserImage}`}
                 alt="avatar"
+                className="user_img"
               />
             </div>
             <div className="username">
@@ -150,6 +151,43 @@ const Container = styled.div`
           font-size: 1rem;
         }
       }
+    }
+  }
+  @media (max-width: 768px) {
+    .contacts {
+      width: 100%;
+    }
+    .brand {
+      display: block;
+    }
+
+    .contact {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      gap: 5px !important;
+    }
+
+    h3 {
+      white-space: nowrap;
+      font-size: 17px;
+    }
+    .avatar img {
+      height: 3rem !important;
+    }
+    .current-user {
+      align-items: start !important;
+      flex-direction: column;
+      gap: 0;
+    }
+    .user_img {
+      margin-left: 20px !important;
+    }
+    .username {
+      margin-left: 10px;
+    }
+    .main_logo {
+      width: 100%;
     }
   }
 `;
